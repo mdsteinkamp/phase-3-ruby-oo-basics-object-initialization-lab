c5ecc9b8916aa2a80c2072d7e97c5a57e848a5fa
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-05-10 17:15:22 -0700

    commit tree db1b0b47a910328997e22360fed3b1e48c4bf721

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  attr_reader :name
+  attr_reader :breed
+
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
+
+lassie = Dog.new("Lassie")
+puts lassie.name
+puts lassie.breed
